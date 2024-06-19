267cfd09889aaa8b6868091d7f54e22293310f20
Jan Kamiński <jan.kaminski@medandlife.com>
2024-06-19 12:41:49 +0200

    added prerelease gh action script

---
+++ .github-scripts/prerelease.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# This script is used to prepare release of a new version of the project.
+
+git submodule update --init --recursive
+
+REPO_NAME=${GITHUB_REPOSITORY##*/}
+TAG=$(git describe --tags --abbrev=0)
+
+echo "Preparing release $TAG of $REPO_NAME"
+
+rm -rf $REPO_NAME-$TAG.zip
+zip -r $REPO_NAME-$TAG.zip ./ -x "*/.git/*" "*/.github/*" "*/.github-scripts/*" ".gitmodules"
+
+gh release create "$TAG" \
+    --repo="$GITHUB_REPOSITORY" \
+    --title="${TAG} ${tag#v}" \
+    --generate-notes \
+    --prerelease
+
+gh release upload "$TAG" $REPO_NAME-$TAG.zip
