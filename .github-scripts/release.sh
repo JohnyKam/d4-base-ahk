fde97a09ecae2a17819ffe87b9739e994b7869dd
Jan Kamiński <jan.kaminski@medandlife.com>
2024-06-19 11:28:49 +0200

    added bash script

---
+++ .github-scripts/release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This script is used to prepare release of a new version of the project.
+
+git submodule update --init --recursive
+
+REPO_NAME=$(git remote show origin | grep "Fetch URL:" | sed "s/.*\/\([^ ]*\)\.git/\1/")
+TAG=$(git describe --tags --abbrev=0)
+
+rm -rf $REPO_NAME-$TAG.zip
+zip -r $REPO_NAME-$TAG.zip ./ -x "*/.git/*" "*/.github/*" "*/.github-scripts/*" ".gitmodules"
