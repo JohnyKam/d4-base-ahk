e35ca84312e7a54b8a2c5f789c88c1fbe826b0ef
Jan Kamiński <jan.kaminski@medandlife.com>
2024-06-19 12:04:10 +0200

    script tests

--- .github-scripts/release.sh
+++ .github-scripts/release.sh
@@ -4,10 +4,9 @@
 
 git submodule update --init --recursive
 
-REPO_NAME=$(git remote show origin | grep "Fetch URL:" | sed "s/.*\/\([^ ]*\)\.git/\1/")
 TAG=$(git describe --tags --abbrev=0)
 
-echo "Preparing release $TAG of $REPO_NAME"
+echo "Preparing release $TAG of $GITHUB_REPOSITORY"
 
 #rm -rf $REPO_NAME-$TAG.zip
 #zip -r $REPO_NAME-$TAG.zip ./ -x "*/.git/*" "*/.github/*" "*/.github-scripts/*" ".gitmodules"
